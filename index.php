b7989dc169b8f35670a097fd12c5782a5e4b0490
RiversHIRSCH <61602942+RiversHIRSCH@users.noreply.github.com>
2020-06-11 16:24:27 -0500

    static

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Reservaciones</title>
+  <!-- jQuery y Popper -->
+  <script src="static/bootstrap/jquery-3.5.1.slim.min.js"></script>
+  <script src="static/bootstrap/popper.min.js"></script>
+  <!-- BOOTSTRAP 4.5.0 -->
+  <link rel="stylesheet" href="static/bootstrap/css/bootstrap.css">
+  <script src="static/bootstrap/js/bootstrap.js"></script>
+  <!-- ALERTIFY 1.13.1 -->
+  <link rel="stylesheet" href="static/alertify/css/alertify.css">
+  <link rel="stylesheet" href="static/alertify/css/themes/default.min.css">
+  <script src="static/alertify/alertify.js"></script>
+  <!-- CUSTOM JS -->
+  <script src="static/js/main.js"></script>
+</head>
+
+<body>
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</body>
+
+</html>
